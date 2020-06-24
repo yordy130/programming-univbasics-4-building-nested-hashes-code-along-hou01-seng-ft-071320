c9ac5e2907c8a28db28a3aea0ae0bd33aa4e0058
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-24 16:55:02 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -15,7 +15,12 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{
+        name: "Steven", age: "30", attitude: "confused"
+      },
+      {
+        name: "Nurse", age: "44", attitude: "worried"
+      }
         #your hashes here!
       ]
    }
