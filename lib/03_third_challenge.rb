aced6f1f1dc9cc82f98bd0571ee0b9901a2154c4
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-24 16:47:07 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,18 +2,18 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Montague"
+        name: "Lord Montague",
         age: "53"
         #your key/value pairs here
         },
       :matriarch => {
-        name: "Lady Montague"
+        name: "Lady Montague",
         age: "54"
         #your key/value pairs here
         },
       :hero => {
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
         #your key/value pairs here
         },
@@ -21,18 +21,18 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "50"
         #your key/value pairs here
         },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
         #your key/value pairs here
         },
       :heroine => {
-        name: "Juliet"
-        age: "15"
+        name: "Juliet",
+        age: "15",
         status: "alive"
         #your key/value pairs here
         },
